fd04a7d017b344ebc37c1f7c8ddafb56c58a9e03
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-05 22:53:07 +0530

    done

--- src/com/company/Premium.java
+++ src/com/company/Premium.java
@@ -1,5 +1,17 @@
 package com.company;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//  Target specifies where this annotation can be useful
+@Target(ElementType.TYPE)   //  to classes, interfaces, and enums
+// for multiple specifications use @Target({ElementType.TYPE, ElementType.METHOD})
+
+//  Retention specifies how deep this annotation is available
+@Retention(RetentionPolicy.CLASS)   //  bytecode deep;
+// others: SOURCE (stripped in bytecode), RUNTIME (even JVM can see, as deep as possible)
 public @interface Premium {
     //  declaring annotation parameters
     int price() default 100;
